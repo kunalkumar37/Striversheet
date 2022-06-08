2294440b5396de00dde45946c93f75aa59a3aef5
Kunal  Kumar <kumarkunal4052002@gmail.com>
2022-06-08 18:32:20 +0530

    first commit

---
+++ 012sort.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+        void sortcolors(vector<int>&nums){
+            int lo=0;
+            int hi=nums.size()-1;
+            int mid=0;
+            while(mid<=hi){
+                switch(nums[mid]){
+                    case 0:
+                        swap(nums[lo],nums[mid]);
+                        lo++;
+                        mid++;
+                        break;
+                    
+                    case 1:
+                        mid++;
+                        break;
+
+                    case 2:
+                        swap(nums[mid],nums[hi]);
+                        hi--;
+                        break;
+                }
+            }
+        }
+};
